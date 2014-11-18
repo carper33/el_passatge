cf5bc03021af230e901835cce8025a7b93cd3851
carper33 <carla.lahoz.perez@gmail.com>
2014-11-18 12:37:22 +0000

    migrations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,8 @@
 class Booking < ActiveRecord::Base
+
+	 validates :pax, presence: true
+	 validates :date_arrival, presence: true
+	 validates :date_departure, presence: true
+	 validates :final_price, numericality: true, presence: true
+	 validates :room_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118115919) do
+ActiveRecord::Schema.define(version: 20141112170618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: true do |t|
     t.integer  "room_id"
+    t.integer  "user_id"
+    t.integer  "pax"
+    t.date     "date_arrival"
     t.date     "date_departure"
     t.integer  "final_price"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.date     "date_arrival"
-    t.integer  "pax"
-    t.integer  "user_id"
   end
 
   create_table "rooms", force: true do |t|
