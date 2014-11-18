d0aecc34a1ed454f7ad420326620a89ec2a252b6
vagrant <vagrant@ironhack>
2014-11-18 15:29:10 +0000

    fix migration

--- db/migrate/20141118104250_update_bookings.rb
+++
@@ -1,8 +0,0 @@
-class UpdateBookings < ActiveRecord::Migration
-  def change
-  	remove_column :bookings, :user_id
-  	remove_column :bookings, :date_arrive
-  	add_column :bookings, :date_arrival, :date
-  	add_column :users, :booking_id, :integer
-  end
-end
