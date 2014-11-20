9216a228ac7cf471213d978074aeb233105f8e02
carper33 <carla.lahoz.perez@gmail.com>
2014-11-20 09:19:09 +0000

    render_form

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,18 @@
+class BookingsController < ApplicationController
+	helper_method :booking
+
+	def index
+			@booking= Booking.new
+	end
+	def create
+		
+
+	end
+
+
+
+	private
+	def booking
+		@booking= Booking.new(params)
+	end
+end
