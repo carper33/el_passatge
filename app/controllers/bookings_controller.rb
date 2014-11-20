8e84b4c46409165f59b987d14e978fb7337ca0e4
Diego <info@diego-suarez.es>
2014-11-20 16:57:39 +0100

    create users controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,18 @@
 class BookingsController < ApplicationController
 	helper_method :booking
 
-	def index
+	def new
 		@booking = Booking.new
 	end
 	def create
 		@booking = Booking.new booking_params
+    @user = User.new
+    render :action "new", controller: "user"
 	end
-
 
 	private
 
 	def booking_params
-		params.require(:booking).permit(:pax, :date_arrival, :date_departure)
+		params.require(:booking).permit(:pax, :date_arrival, :date_departure, :room_id)
 	end
 end
